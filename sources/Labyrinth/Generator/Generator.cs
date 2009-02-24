e3279c0168ab823bbf6049689aad92bdd3b86140
kindex <kindex@99e027b2-380c-0410-a737-a5784e1a7bde>
2009-02-24 19:14:46 +0000

    labyrinth generator
    git-svn-id: https://kindex.lv/division_svn/steel/trunk/labyrinth2@715 99e027b2-380c-0410-a737-a5784e1a7bde

---
+++ sources/Labyrinth/Generator/Generator.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Labyrinth.Generator
+{
+    static class Generator
+    {
+        sealed class LabyrinthGenerator
+        {
+            class Node
+            {
+                public int dist;
+                public bool path;
+                public bool rightborder;
+                public bool upborder;
+                public bool was;
+            }
+            Node[,] matrix;
+            static Random random = new Random();
+
+            public LabyrinthGenerator(int x, int y)
+            {
+                matrix = new Node[x, y];
+                for (int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                    {
+                        matrix[i, j] = new Node();
+                        matrix[i, j].dist = int.MaxValue;
+                    }
+                }
+            }
+
+            public void Build(float density)
+            {
+                if (dim_x > 0 && dim_y > 0)
+                {
+	                Deep(0, 0, 0);
+                }
+	            //findpath(X-1, Y-1);
+	            //findpath(X-1, 0);
+	            //findpath(0, Y-1);
+
+                //string d = "\n";
+
+                //for(int i = 0; i < dim_y; i++)
+                //{
+                //    d += "|";
+                //    for(int j = 0; j < dim_x; j++)
+                //    {
+                //        d += "\t" + IntToStr(a[i][j].dist);
+                //    }
+                //    d += "|\n";
+                //}
+                //log_msg("labyrinth", d);
+
+	            for(int y = 0; y < dim_y; y++)
+	            {
+		            for(int x = 0; x < dim_x-1; x++)
+		            {
+                        matrix[x, y].rightborder = random.NextDouble() > density && Math.Abs(matrix[x, y].dist - matrix[x + 1, y].dist) != 1;
+		            }
+	            }
+	            for(int y = 0; y < dim_y-1; y++)
+	            {
+		            for(int x = 0; x < dim_x; x++)
+		            {
+                        matrix[x, y].upborder = random.NextDouble() > density && Math.Abs(matrix[x, y].dist - matrix[x, y + 1].dist) != 1;
+		            }
+	            }
+            }
+
+            public Matrix getMatrix()
+            {
+                Matrix result = new Matrix(matrix.GetLength(0), matrix.GetLength(1));
+                for (int x = 0; x < dim_x; x++)
+                {
+                    for (int y = 0; y < dim_y; y++)
+                    {
+                        result.setBorders(x, y, matrix[x, y].rightborder, matrix[x, y].upborder);
+                    }
+                }
+
+                return result;
+            }
+
+            public int dim_y
+            {
+                get
+                {
+                    return matrix.GetLength(0);
+                }
+            }
+
+            public int dim_x
+            {
+                get
+                {
+                    return matrix.GetLength(1);
+                }
+            }
+
+            bool Deep(int x, int y, int level)
+            {
+                Node current = matrix[x, y];
+	            if (current.was)
+	            {
+		            return false;
+	            }
+	            current.dist = level;
+	            current.was = true;
+	            if (x == dim_x-1 && y == dim_y-1)
+	            {
+		            return true;
+	            }
+
+	            bool[] used_directions = {false, false, false, false};
+	            int total = 0;
+
+	            while (total < 4)
+	            {
+		            int d;
+		            do
+		            {
+                        d = random.Next(0, 5) % 4;
+		            }
+		            while (used_directions[d]);
+		            total++;
+
+		            used_directions[d] = true;
+
+		            switch(d)
+		            {
+			            case 0: 
+				            if (x > 0)
+				            {
+					            Deep(x - 1, y, level + 1);
+				            }
+				            break;
+			            case 1:
+				            if (y > 0)
+				            {
+					            Deep(x, y - 1, level + 1); 
+				            }
+				            break;
+			            case 2:
+				            if (x < dim_x-1)
+				            {
+					            Deep(x + 1, y, level + 1);
+				            }
+				            break;
+			            case 3:
+				            if (y < dim_y-1)
+				            {
+					            Deep(x, y + 1, level + 1); 
+				            }
+				            break;
+		            }
+	            }
+	            return false;
+            }
+
+            void findpath(int x, int y)
+            {
+                Node cur_node = matrix[x, y];
+                cur_node.path = true;
+                int d = cur_node.dist - 1;
+                if (y > 0 && matrix[x, y - 1].dist == d) findpath(x, y - 1);
+                else if (x > 0 && matrix[x - 1, y].dist == d) findpath(x - 1, y);
+                else if (y < dim_y - 1 && matrix[x, y + 1].dist == d) findpath(x, y + 1);
+                else if (x < dim_x - 1 && matrix[x + 1, y].dist == d) findpath(x + 1, y);
+            }
+
+        }
+
+        public static Matrix Generate(int x, int y, float density)
+        {
+            LabyrinthGenerator generator = new LabyrinthGenerator(x, y);
+            generator.Build(density);
+
+            return generator.getMatrix();
+        }
+    }
+}
---
+++ sources/Labyrinth/Generator/Matrix.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Labyrinth.Generator
+{
+    class Matrix
+    {
+	    struct Node
+	    {
+		    public bool right;
+            public bool up;
+	    };
+
+        // first index - x, second - y
+        Node[,] matrix;
+
+        public Matrix(int x, int y)
+        {
+            matrix = new Node[x, y];
+        }
+
+        public bool isRightBorder(int x, int y)
+        {
+            if (!(x < dim_x && y >= 0))
+            {
+                return false;
+            }
+            if ((x < 0 || y < 0))
+            {
+                return true;
+            }
+            return y + 1 > dim_y
+                || x + 1 >= dim_x
+                || matrix[x, x].right;
+        }
+
+        public bool isUpBorder(int x, int y)
+        {
+            if (!(y < dim_y && x >= 0))
+            {
+                return false;
+            }
+            if (x < 0 || y < 0)
+            {
+                return true;
+            }
+            return y + 1 >= dim_y
+                || x >= dim_x
+                || matrix[x, y].up;
+        }
+
+	    public int dim_y
+        {
+            get
+            {
+                return matrix.GetLength(0);
+            }
+        }
+
+        public int dim_x
+        {
+            get
+            {
+                return matrix.GetLength(1);
+            }
+        }
+
+        public void setBorders(int x, int y, bool right, bool up)
+        {
+            matrix[x, y].right = right;
+            matrix[x, y].up = up;
+        }
+    }
+}
+
+
+
+//class Labyrinth
+//{
+//public:
+//    bool isRightBorder(int x, int y) const;
+//    bool isUpBorder(int x, int y) const;
+//    int getMaxX() const;
+//    int getMaxY() const;
+//    void setSize(size_t x, size_t y);
+
+//    friend class LabyrinthGenerator;
+//    std::string serialize() const;
+//    void unserialize(int x, int y, const std::string& data);
+
+//private:
+//    struct node
+//    {
+//        bool rightborder;
+//        bool upborder;
+//    };
+
+//// first index - y, second - x
+
+//    std::vector<std::vector<node> > a;
+//};
